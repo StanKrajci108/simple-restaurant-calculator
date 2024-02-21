cc425c622719e6d4d77efb8a7526d57316457097
StanKrajci108 <krajci.s@tutanota.com>
2024-02-21 18:42:17 +0100

    commit

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// // Retrieving the "result" class from HTML
-// const section = document.querySelector(".result");
-
-// // Getting data from the user
-// const spending = parseFloat(
-//   prompt("How much will it be for dinner together??")
-// );
-// const tip = parseFloat(prompt("What percentage will they tip?"));
-// const people = parseInt(prompt("How many people will pay?"));
-
-// // Calculation of how much a person will pay for food
-// const result = ((spending + spending * (tip / 100)) / people).toFixed(0);
-// console.log(result);
-
-// // Creating a new paragraph, filling it and adding it to the page
-// const paragraph = document.createElement("p");
-// paragraph.textContent = `Each of the ${people} people will pay ${result} €.`;
-// section.prepend(paragraph);
-
 const form = document.querySelector("#dinnerForm");
 const section = document.querySelector(".result");
 
@@ -35,3 +16,6 @@ form.addEventListener("submit", function (event) {
 
   section.style.display = "block";
 });
+
+const spendingInput = document.querySelector("#spending");
+const tipInput = document.querySelector("#tip");
